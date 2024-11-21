ed4e89abce232b58d70a2a3ca4c11d96c8c29024
22120197 <luongnguyen02042004@gmail.com>
2024-11-21 10:56:49 +0700

    update host

--- src/public/js/vue-app.js
+++ src/public/js/vue-app.js
@@ -3,7 +3,7 @@ new Vue({
     el: '#app',
     delimiters: ['[[', ']]'],
     data: {
-        DOMAIN: 'https://web-comic-3.onrender.com/',
+        DOMAIN: 'https://web-comic-g4cg.onrender.com/',
         //DOMAIN: 'http://localhost:8888/',
         domain_image: 'https://img.otruyenapi.com',
         domain_cdn_read: '',
