1e21dc79ecdf0c33f18f59cbf0e021453f2df262
22120197 <luongnguyen02042004@gmail.com>
2024-11-24 00:42:12 +0700

    fix

--- src/app/utils/AuthPass.js
+++ src/app/utils/AuthPass.js
@@ -1,27 +1,26 @@
-const bcrypt = require('bcrypt'); // Băm mật khẩu ( 1 chiều ) 
+const bcrypt = require('bcrypt'); // Băm mật khẩu (1 chiều)
 
-// Xác thực mật khẩu với mật khẩu đã băm
-async function verifyPassword(password, hashedPassword) {
-    try {
-        const isMatch = await bcrypt.compare(password, hashedPassword);
-        return isMatch;
-    } catch (error) {
-        console.error('Lỗi khi xác thực mật khẩu:', error);
+class AuthPass {
+    // Xác thực mật khẩu với mật khẩu đã băm
+    static async verifyPassword(password, hashedPassword) {
+        try {
+            const isMatch = await bcrypt.compare(password, hashedPassword);
+            return isMatch;
+        } catch (error) {
+            console.error('Lỗi khi xác thực mật khẩu:', error);
+        }
+    }
+
+    // Băm mật khẩu (1 chiều)
+    static async hashPassword(password) {
+        try {
+            const saltRounds = 10; // số vòng băm, càng cao càng bảo mật
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            return hashedPassword;
+        } catch (error) {
+            console.error('Lỗi khi băm mật khẩu:', error);
+        }
     }
 }
 
-// Băm 1 chiều
-async function hashPassword(password) {
-    try {
-        const saltRounds = 10; // số vòng băm, càng cao càng bảo mật
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword;
-    } catch (error) {
-        console.error('Lỗi khi băm mật khẩu:', error);
-    }
-}
-
-module.exports = {
-    verifyPassword,
-    hashPassword
-}
+module.exports = AuthPass;
