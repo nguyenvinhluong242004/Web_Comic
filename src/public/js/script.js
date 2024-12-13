ce687f0f8f7bac9ab5b09e09e8aa9c75cb8a1265
22120197 <luongnguyen02042004@gmail.com>
2024-12-13 22:31:03 +0700

    add btn up

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         nav: true,
         autoplay: true,
         autoplayTimeout: 3000,
-      
+
     });
     $('#txtInput').on('keydown', function (event) {
         if (event.key === 'Enter') {
@@ -21,4 +21,41 @@ $(document).ready(function () {
             vueApp.searchComicVer2(); // Gọi hàm tìm kiếm
         }
     });
+
+    $('.btn-up').on('click', function () {
+        console.log('click')
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+
+    let startY = 0; // Vị trí Y khi bắt đầu vuốt
+    let endY = 0;   // Vị trí Y khi kết thúc vuốt
+
+    // Xử lý sự kiện vuốt màn hình (cho thiết bị cảm ứng)
+    $(document).on('touchstart', function (e) {
+        startY = e.originalEvent.touches[0].clientY; // Lấy tọa độ Y lúc bắt đầu
+    });
+
+    $(document).on('touchmove', function (e) {
+        endY = e.originalEvent.changedTouches[0].clientY; // Lấy tọa độ Y lúc kết thúc
+
+        if (startY > endY + 50) {
+            console.log('Vuốt lên');
+            $('.btn-up').hide();
+        } else if (startY < endY - 50) {
+            console.log('Vuốt xuống');
+            $('.btn-up').show();
+        }
+    });
+
+    // Xử lý sự kiện lăn chuột (cho thiết bị desktop)
+    $(document).on('wheel', function (e) {
+        if (e.originalEvent.deltaY > 0) {
+            console.log('Lăn xuống');
+            $('.btn-up').show();
+        } else if (e.originalEvent.deltaY < 0) {
+            console.log('Lăn lên');
+            $('.btn-up').hide();
+        }
+    });
+
 });
